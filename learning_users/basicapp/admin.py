ed43c91d34bfe5b602586c0ca82985aeaad671f4
ArunKP <arunkp1987@gmail.com>
2020-09-21 00:54:07 +0530

    'first'

---
+++ learning_users/basicapp/admin.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from basicapp.models import UserProfileInfo
+
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.register(UserProfileInfo)
+
+
